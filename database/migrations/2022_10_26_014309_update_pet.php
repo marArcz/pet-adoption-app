4672049b392c78a951caeaaaf485a450a7d31e04
marArcz <mar19arcz@gmail.com>
2022-10-31 19:56:51 +0800

    updates

--- database/migrations/2022_10_26_014309_update_pet.php
+++ database/migrations/2022_10_26_014309_update_pet.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::table('pets', function (Blueprint $table) {
             //
-            $table->integer('status')->default(1)->change();
+            $table->string('status')->default(1)->change();
         });
     }
 
